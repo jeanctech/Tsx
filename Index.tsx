a5c8fb5331a3c53e1566dcfdcd8c79336e139dd6
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2023-12-13 06:31:03 -0500

    Changes Initial

---
+++ Index.tsx
@@ -0,0 +1,13 @@
+import React from react
+
+function app (){
+    return(
+        <>
+        <div className="app">
+            <h1>App</h1>
+        </div>
+        </>
+    )
+}
+
+export default App
